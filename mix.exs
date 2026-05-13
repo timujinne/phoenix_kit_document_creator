02a4c5363842467fed31727c008b84435d5c1381
Dmitri Don <fotkin@gmail.com>
2026-05-13 19:43:20 +0000

    precommit improved

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule PhoenixKitDocumentCreator.MixProject do
     [
       quality: ["format", "credo --strict", "dialyzer"],
       "quality.ci": ["format --check-formatted", "credo --strict", "dialyzer"],
-      precommit: ["compile", "quality"]
+      precommit: ["compile --force --warnings-as-errors", "deps.unlock --check-unused", "quality.ci"]
     ]
   end
 
